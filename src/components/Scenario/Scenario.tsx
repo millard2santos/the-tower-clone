11c28ef57aa44ec1a7ea58af3058a12babbb2f41
Millard Dos Santos <millard2santos@gmail.com>
2024-08-19 13:39:04 +0100

    Create Scenario

--- src/components/Scenario/Scenario.tsx
+++ src/components/Scenario/Scenario.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { usePlayer } from '../../context/PlayerContext'
 
 type Props = {}
 
 const Scenario = (props: Props) => {
+
+    const {player} = usePlayer()
+    const [currentPlayer, setCurrentPlayer] = useState(player.clone())
+
+
+
   return (
     <div>Scenario</div>
   )
